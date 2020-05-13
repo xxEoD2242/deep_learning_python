65d4a4936649f023b14ca83f96c59610adb0d4d4
xxEoD2242 <tyler.fedrizzi@gmail.com>
2020-05-13 10:03:42 -0400

    Complete chapter 5

---
+++ convnets/dogs_and_cats_vgg16_fast.py
@@ -0,0 +1,69 @@
+from keras.applications import VGG16
+import os
+import numpy as np
+from keras.preprocessing.image import ImageDataGenerator
+from keras import models, layers, optimizers
+from plotting_utils import plot_metric
+
+def extract_features(directory, sample_count):
+	features = np.zeros(shape=(sample_count, 4, 4, 512))
+	labels = np.zeros(shape=(sample_count))
+	generator = datagen.flow_from_directory(
+		directory,
+		target_size=(150, 150),
+		batch_size=batch_size,
+		class_mode='binary')
+	i = 0
+	for inputs_batch, labels_batch in generator:
+		features_batch = conv_base.predict(inputs_batch)
+		features[i * batch_size : (i + 1) * batch_size] = features_batch
+		labels[i * batch_size : (i + 1) * batch_size] = labels_batch
+		i += 1
+		if i * batch_size >= sample_count:
+			break
+	return features, labels
+
+base_dir = os.getcwd()
+base_dir = os.path.join(base_dir, 'dogs_vs_cats_small')
+train_dir = os.path.join(base_dir, 'train')
+validation_dir = os.path.join(base_dir, 'validation')
+test_dir = os.path.join(base_dir, 'test')
+
+datagen = ImageDataGenerator(rescale=1./255)
+batch_size = 20
+
+conv_base = VGG16(weights='imagenet',
+				  include_top=False,
+				  input_shape=(150,150,3))
+
+train_features, train_labels = extract_features(train_dir, 2000)
+validation_features, validation_labels = extract_features(validation_dir, 1000)
+test_features, test_labels = extract_features(test_dir, 1000)
+
+train_features = np.reshape(train_features, (2000, 4 * 4 * 512))
+validation_featrures = np.reshape(validation_features, (1000, 4 * 4 * 512))
+test_features = np.reshape(test_features, (1000, 4 * 4 * 512))
+
+model = models.Sequential()
+model.add(layers.Dense(256, activation='relu', input_dim=4 * 4 * 512))
+model.add(layers.Dropout(0.5))
+model.add(layers.Dense(1, activation='sigmoid'))
+
+model.compile(optimizer=optimizers.RMSprop(lr=2e-5),
+			  loss='binary_crossentropy',
+			  metrics=['acc'])
+
+val_data = tuple([validation_featrures, validation_labels])
+
+history = model.fit(train_features, train_labels,
+					epochs=30,
+					batch_size=20,
+					validation_data=val_data)
+
+acc = history.history['acc']
+val_acc = history.history['val_acc']
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+
+plot_metric(acc, val_acc, 'Accuracy')
+plot_metric(loss, val_loss, 'Loss')
